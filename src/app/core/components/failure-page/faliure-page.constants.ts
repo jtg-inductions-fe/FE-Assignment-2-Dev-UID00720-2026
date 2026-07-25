b0d2b861b9ee2d89e41a6496827b6cf46a0d95e1
DevVermaJTG <dev.verma@joshtechnologygroup.com>
2026-07-25 15:18:25 +0530

    [Develop]: Update images location in assets folder and add netlify configuration for deploying web app

--- src/app/core/components/failure-page/faliure-page.constants.ts
+++ src/app/core/components/failure-page/faliure-page.constants.ts
@@ -2,11 +2,11 @@ export const data = {
   pageNotFound: {
     heading: 'Page not found',
     desc: 'Oops! Looks like you followed a bad link. If you think this is a problem with us, please tell us.',
-    img: 'assets/404.svg',
+    img: 'assets/images/404.svg',
   },
   errorPage: {
     heading: 'Something has gone seriously wrong',
     desc: "It's always time for a coffee break We should be back by the time you finish your coffee.",
-    img: 'assets/error-page-img.svg',
+    img: 'assets/images/error-page-img.svg',
   },
 };
